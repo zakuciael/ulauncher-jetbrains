48cc26fe24726fc8e630c0c80281f72dab5b4031
SavKS <sav1991@gmail.com>
2020-11-14 19:42:00 +0200

    feat: Show project title if exists (#25)
    Co-authored-by: Andrii Savluk <a.savluk@ideil.com>

--- jetbrains/project_parser.py
+++ jetbrains/project_parser.py
@@ -30,16 +30,23 @@ class RecentProjectsParser():
 
         result = []
         for project in recent_projects:
+            project_title = ''
             project_path = project.attrib["value"].replace(
                 '$USER_HOME$', os.path.expanduser('~'))
+            name_file = project_path + '/.idea/.name'
+
+            if os.path.exists(name_file):
+                with open(name_file, 'r') as file:
+                    project_title = file.read().replace('\n', '')
+
             project_name = os.path.basename(project_path)
             icons = glob.glob(os.path.join(project_path, '.idea', 'icon.*'))
 
-            if query and query.lower() not in project_name.lower():
+            if query and query.lower() not in project_name.lower() and query.lower() not in project_title.lower():
                 continue
 
             result.append({
-                'name': project_name,
+                'name': project_title or project_name,
                 'path': project_path,
                 'icon': icons[0] if len(icons) > 0 else None
             })
