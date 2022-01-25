350ea9c0ff36e4db51eb150226fb44529de1cae5
Krzysztof Saczuk <zakku@zakku.eu>
2022-01-25 23:55:33 +0100

    refactor(KeywordQueryEventListener): replace .format with f-string

--- events/KeywordQueryEventListener.py
+++ events/KeywordQueryEventListener.py
@@ -71,7 +71,7 @@ class KeywordQueryEventListener(EventListener):
                     name=project.name,
                     description=project.path,
                     on_enter=RunScriptAction(
-                        '{} "{}" &'.format(extension.get_ide_launcher_script(project.ide), project.path)
+                        f'{extension.get_ide_launcher_script(project.ide)} "{project.path}" &'
                     ),
                     on_alt_enter=CopyToClipboardAction(project.path)
                 )
